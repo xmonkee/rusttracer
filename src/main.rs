c9c07eb201195b2ca9db4fe60e57ac865d30f772
mandava <mandava@uber.com>
2017-02-12 18:38:03 -0800

    matt shading

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ static HORIZONTAL:Vec3 = Vec3 {e: [4f32, 0f32, 0f32]};
 
 fn main() {
     let mut img: Image = Image::new(NX, NY);
-    let mut camera = Camera::new();
     //let scene = blank::Blank::new();
     let sphere = sphere::Sphere::new(
         Vec3::new(0f32, 0f32, -1f32), 
@@ -41,19 +40,38 @@ fn main() {
     let mut scene = list::List::new();
     scene.add(sphere);
     scene.add(sphere2);
+
+    let mut camera = Camera::new(scene);
+
     for (x, y) in img.coordinates() {
-        img.set_pixel(x, y, camera.color(x, y, &scene).to_pixel())
+        img.set_pixel(x, y, camera.color(x, y).to_pixel())
     }
     let _ = img.save("img.bmp");
 }
 
 struct Camera {
-    rng: ThreadRng
+    rng: ThreadRng,
+    scene: Box<Hitable>,
 }
 
 impl Camera {
-    fn new() -> Camera {
-        Camera{rng: rand::thread_rng()}
+    fn new<H: Hitable + 'static>(scene: H) -> Camera {
+        Camera{
+            rng: rand::thread_rng(),
+            scene: Box::new(scene),
+        }
+    }
+
+    fn random_in_unit_sphere(&mut self) -> Vec3 {
+        let mut p = Vec3::new(1f32, 1f32, 1f32);
+        while p.lensq() >= 1.0 {
+            p = Vec3::new(
+                self.rng.gen::<f32>(),
+                self.rng.gen::<f32>(),
+                self.rng.gen::<f32>(),
+            )
+        }
+        return p;
     }
 
     fn ray_from_origin(&mut self, x: u32, y:u32) -> Ray {
@@ -64,28 +82,28 @@ impl Camera {
         Ray::new(ORIGIN, LOWER_CORNER - ORIGIN + u + v)
     }
 
-    fn color<H: Hitable>(&mut self, x: u32, y: u32, scene: &H) -> Vec3 {
+    fn color(&mut self, x: u32, y: u32) -> Vec3 {
         let mut c = Vec3::new(0f32, 0f32, 0f32);
 
         let ns = 10;
         for _ in 0..ns {
             let ray = &self.ray_from_origin(x, y);
-            let hit = scene.hit(0.0, 1000.0, ray);
-            c = c + self.color1(ray, hit);
+            c = c + self.color1(ray);
         }
         c / ns as f32
     }
 
-    fn color1(&self, ray : &Ray, hit : Option<HitRecord>) -> Vec3 {
+    fn color1(&mut self, ray : &Ray) -> Vec3 {
+        let hit = self.scene.hit(0.0, 1000.0, ray);
         match hit {
+            Some(t) => {
+                let target = t.p + t.normal + self.random_in_unit_sphere();
+                self.color1(&Ray::new(t.p, target-t.p)) * 0.5 
+            },
             None => {
                 let y_cord = ray.direction().y();
                 let t = 0.5 * VY * (y_cord + VY);
                 BLUE * (1.0 - t) + WHITE * t
-            },
-            Some(t) => {
-                let norm = t.normal;
-                (norm + Vec3::new(1f32, 1f32, 1f32)) * 0.5
             }
         }
     }
