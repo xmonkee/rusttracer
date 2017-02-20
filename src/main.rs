0748deb426a4f8e54b98bbf538ca4f911d0fd629
mandava <mandava@uber.com>
2017-02-19 21:54:51 -0800

    Add metals

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ extern crate ray;
 extern crate rand;
 use ray::vec3::{Vec3, Ray};
 use ray::scenes::*;
-use ray::materials::Lambertian;
+use ray::materials::{Lambertian, Metal};
 use ray::{Hitable};
 use rand::{ThreadRng, Rng};
 use bmp::{Image, Pixel};
 
 /* Actual canval pixel size */
-const NX:u32 = 400;
-const NY:u32 = 200;
+const NX:u32 = 600;
+const NY:u32 = 300;
 
 /* Virtual canval size limits 
  * X axis will go from -VX to VX and so on */
@@ -31,21 +31,22 @@ static HORIZONTAL:Vec3 = Vec3 {e: [4f32, 0f32, 0f32]};
 fn main() {
     let mut img: Image = Image::new(NX, NY);
     //let scene = blank::Blank::new();
-    let material = Lambertian::new(Vec3::new(0.5f32, 0.5f32, 0.5f32));
+    //
+    let material0 = Lambertian::new(Vec3::new(0.8 , 0.3 , 0.3));
+    let material1 = Lambertian::new(Vec3::new(0.3 , 0.8 , 0.0));
+    let material2 = Metal::new(Vec3::new(0.8      , 0.6 , 0.2));
+    let material3 = Metal::new(Vec3::new(0.8      , 0.8 , 0.8));
 
-    let sphere = sphere::Sphere::new(
-        Vec3::new(0f32, 0f32, -1f32), 
-        0.5f32,
-        &material,
-    );
-    let sphere2 = sphere::Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        &material,
-    );
+
+    let sphere0 = sphere::Sphere::new(Vec3::new(0.0  , 0.0    , -1.0) , 0.5   , &material0);
+    let sphere1 = sphere::Sphere::new(Vec3::new(0.0  , -100.5 , -1.0) , 100.0 , &material1);
+    let sphere2 = sphere::Sphere::new(Vec3::new(1.0  , 0.0    , -1.0) , 0.5   , &material2);
+    let sphere3 = sphere::Sphere::new(Vec3::new(-1.0 , 0.0    , -1.0) , 0.5   , &material3);
     let mut scene = list::List::new();
-    scene.add(&sphere);
+    scene.add(&sphere0);
+    scene.add(&sphere1);
     scene.add(&sphere2);
+    scene.add(&sphere3);
 
     let mut camera = Camera::new(&scene);
 
@@ -79,7 +80,7 @@ impl <'a> Camera <'a> {
     fn color(&mut self, x: u32, y: u32) -> Vec3 {
         let mut c = Vec3::new(0f32, 0f32, 0f32);
 
-        let ns = 10;
+        let ns = 40;
         for _ in 0..ns {
             let ray = &self.ray_from_origin(x, y);
             c = c + self.color1(&ray, 0);
--- src/materials.rs
+++ src/materials.rs
@@ -7,6 +7,10 @@ pub struct Lambertian {
     albedo: Vec3,
 }
 
+pub struct Metal {
+    albedo: Vec3,
+}
+
 impl Lambertian {
     pub fn new(albedo: Vec3) -> Lambertian {
         Lambertian{
@@ -16,12 +20,38 @@ impl Lambertian {
 
 }
 
+impl Metal {
+    pub fn new(albedo: Vec3) -> Metal {
+        Metal{
+            albedo: albedo,
+        }
+    }
+
+}
+
+
 impl Material for Lambertian {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut ThreadRng) -> (Vec3, Option<Ray>) {
         let target = rec.p + rec.normal + random_in_unit_sphere(rng);
         let scattered = Ray::new(rec.p, target - rec.p);
         (self.albedo, Some(scattered))
     }
+}
+
+impl Material for Metal {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut ThreadRng) -> (Vec3, Option<Ray>) {
+        let reflected= reflect(r_in.direction().unit_vector(), rec.normal);
+        let scattered = Ray::new(rec.p, reflected);
+        if scattered.direction().dot(&rec.normal) > 0f32 {
+            (self.albedo, Some(scattered))
+        } else {
+            (self.albedo, None)
+        }
+    }
+}
+
+fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    v - n * v.dot(&n) * 2f32
 }
 
 fn random_in_unit_sphere(rng: &mut ThreadRng) -> Vec3 {
